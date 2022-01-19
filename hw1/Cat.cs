290fcdbe93e826035080d274070be7f57fd11fbe
andjnewb <andjnewb@EPSG1-825030>
2022-01-18 19:43:23 -0600

    Add project files.

---
+++ hw1/Cat.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw1
+{
+    public class Cat
+    {
+        private string name;
+
+        public Cat()
+        {
+            name = "Steve";
+        }
+
+        public Cat(string n)
+        {
+            name = n;
+        }
+
+        public void printCat()
+        {
+            Console.WriteLine("This cat's name is " + name);
+        }
+    }
+}
---
+++ hw1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace hw1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Program p = new Program();
+
+            p.sayGoodbye();
+
+            Cat c = new Cat();
+            Cat j = new Cat("Philbert");
+            c.printCat();
+            j.printCat();
+            Console.ReadKey();
+        }
+
+        void sayGoodbye()
+        {
+            Console.WriteLine("Goodbye World!");
+        }
+    }
+
+
+}
